77a62deed0326180df3421bc2a1901808ddd28f4
Dibyaranjan Nayak <kumardibya20@gmail.com>
2026-04-21 22:40:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ const allowedOrigins = [
     "http://localhost:5174",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+    "https://ai-interview-client-tahg.onrender.com",
     process.env.CLIENT_URL,
 ].filter(Boolean)
 
